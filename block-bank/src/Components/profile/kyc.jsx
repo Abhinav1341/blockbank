1e521a8566651cbc4e72b7dbba37c768f9edd99e
dev04sa <devsa2067@gmail.com>
2024-11-08 20:18:45 +0530

    issue resolved

--- block-bank/src/Components/profile/kyc.jsx
+++ block-bank/src/Components/profile/kyc.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../navbar";
 
 function KYC() {
   const [userData, setUserData] = useState({
