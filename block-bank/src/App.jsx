cabf802cd2326dcf6fcfc4ada53edee38a35cd13
Abhinav <abhinavsinghhg@gmail.com>
2024-11-08 13:02:33 +0530

    Post Login Page Initializatino

--- block-bank/src/App.jsx
+++ block-bank/src/App.jsx
@@ -1,9 +1,5 @@
-
+import HomePage from "./Components/home";
 
 export default function App() {
-  return (
-    <h1 className="text-3xl text-red-400 font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <HomePage />;
 }
